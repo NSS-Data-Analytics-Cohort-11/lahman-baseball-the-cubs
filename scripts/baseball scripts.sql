aa4fa468e178c31e61d8fe7a984008e5753ef431
eemchadwick <eem.chadwick@gmail.com>
2024-02-29 20:31:24 -0600

    on question 6

--- scripts/baseball scripts.sql
+++ scripts/baseball scripts.sql
@@ -17,6 +17,7 @@ LIMIT 10;
 --
 -- SELECT *
 -- FROM people;
+
 SELECT CONCAT(namefirst,' ', namelast) AS name, height, g_all AS games_played, name AS team_name
 FROM people
 INNER JOIN appearances
@@ -30,26 +31,29 @@ LIMIT 1;
 
 --question 3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
 
---schools table: schoolid, schoolname
---people: namefirst, namelast
---salaries: salary
+--correct answer:
+SELECT *
+FROM schools
+WHERE schoolname LIKE '%Vander%';
 
---SELECT *
--- FROM collegeplaying
--- LIMIT 10
+WITH vandy_players AS (
+	SELECT DISTINCT playerid
+	FROM collegeplaying
+	WHERE schoolid = 'vandy'
+)
+SELECT 
+	namefirst ||  namelast AS fullname, 
+	SUM(salary)::int::MONEY AS total_salary
+FROM salaries
+INNER JOIN vandy_players
+USING(playerid)
+INNER JOIN people
+USING(playerid)
+GROUP BY namefirst || namelast
+ORDER BY total_salary DESC;
+--answer: David Price
 
--- SELECT *
--- FROM schools
--- LIMIT 10
-
--- SELECT *
--- FROM people
--- LIMIT 10
-
--- SELECT *
--- FROM salaries
--- LIMIT 10
-
+--wrong answer: 
 SELECT CONCAT(namefirst,' ', namelast) AS name, SUM(salary)::numeric::money
 FROM schools
 INNER JOIN collegeplaying
@@ -59,19 +63,51 @@ USING (playerid)
 INNER JOIN salaries
 USING (playerid) 
 WHERE schoolname LIKE 'Vanderbilt%'
-GROUP BY schoolname, CONCAT(namefirst,' ', namelast)
+GROUP BY CONCAT(namefirst,' ', namelast)
 ORDER BY SUM(salary)::numeric::money DESC;
 --answer: David Price
 
 --question 4: Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
 
-SELECT *
-FROM fielding
-LIMIT 10;
+-- SELECT *
+-- FROM fielding
+-- LIMIT 10;
 
 SELECT SUM(po) AS total_putouts, 
 	CASE WHEN pos = 'OF' THEN 'Outfield'
 		 WHEN pos IN ('SS','1B','2B','3B') THEN 'Infield'
 		 WHEN pos IN ('P', 'C') THEN 'Battery' END AS position
 FROM fielding
-GROUP BY position, pos
+WHERE yearid = 2016
+GROUP BY position;
+--answer: see query
+
+--question 5: Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
+
+
+WITH games_since1920 AS (
+						SELECT SUM(G) AS games_played,
+							10*FLOOR(yearid/10) AS Decade,
+							ROUND(SUM(hr),2) AS AVG_homeruns,
+							ROUND(SUM(so),2) AS AVG_strikeouts
+						FROM teams
+						WHERE yearid >=1920
+						GROUP BY Decade
+						)
+SELECT Decade, 
+		ROUND(AVG_homeruns/games_played,2) AS homeruns_per_game,
+		ROUND(AVG_strikeouts/games_played,2) AS strikeouts_per_game
+FROM games_since1920
+ORDER BY Decade ASC;
+
+--answer: run query; avg. strikeouts per game and homeruns per game increase as time goes on
+
+--question 6: Find the player who had the most success stealing bases in 2016, where __success__ is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted _at least_ 20 stolen bases.
+
+SELECT *
+FROM batting
+
+SELECT playerid, sb, cs, sb/(sb+cs)
+FROM batting
+WHERE yearid = 2016 
+	AND (sb + cs)>= 20
